45c41026e9e5c2fef4ca5527d57695045018dad5
Jérôme L <jerlio@users.noreply.github.com>
2023-08-23 20:09:31 +0200

    feat: flash a player

--- src/Controller/FlashController.php
+++ src/Controller/FlashController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Services\UserFlash;
+use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FlashController extends AbstractController
 {
     #[Route('/flash/{code}', name: 'app_flash')]
-    public function index(string $code): Response
+    public function index(string $code, Security $security, UserFlash $userFlash): Response
     {
+        /** @var User $user */
+        $user = $security->getUser();
+
+        try {
+            $what = $userFlash->flashUser($user, $code);
+        } catch (EntityNotFoundException $e) {
+            
+        }
+
         return $this->render('flash/index.html.twig', [
             'flashedCode' => $code,
         ]);
