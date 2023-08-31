e0256bc58c49de369832b855beef1dad69ff2440
Jérôme L <jerome@jerlio.fr>
2023-08-31 06:51:13 +0200

    feat: add easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
