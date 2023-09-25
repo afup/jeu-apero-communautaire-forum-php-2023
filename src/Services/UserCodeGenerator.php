3dde81d12647e9191ea11a13d552d5ad4fe4f0ba
Adrien Gallou <adriengallou@gmail.com>
2023-09-25 18:54:07 +0200

    on évite un doublon de code

--- src/Services/UserCodeGenerator.php
+++ src/Services/UserCodeGenerator.php
@@ -21,7 +21,7 @@ final class UserCodeGenerator
         $teams = $this->teamRepository->findAllOrderedByName();
 
         for ($i = $first; $i < $last; $i++) {
-            $code = strtr(substr(md5($i . 'SALT_AFUPPPP'), 0, 5), ['0' => 'a', 'o' => 'b', 'i' => 'c', 'l' => 'd']);
+            $code = strtr(substr(md5($i . 'SALT_AFUPPPP'), 0, 5), ['0' => 'e', 'o' => 'b', 'i' => 'c', 'l' => 'd']);
             $team = $teams[$i % count($teams)];
 
             $user = (new User())
