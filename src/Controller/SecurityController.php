1bc18cfd844ec0e3608f4e09ecff39e772483b68
Jérôme L <jerlio@users.noreply.github.com>
2023-10-06 22:04:35 +0200

    changements suite à démo

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,7 +12,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class SecurityController extends AbstractController
